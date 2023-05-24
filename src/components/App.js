2cecefdf5c5ffc74f84535ea6aee29ab0038e672
sanatab <120128180+sanatab@users.noreply.github.com>
2023-05-24 13:26:44 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,31 @@
 
-import React from "react";
-import './../styles/App.css';
+import logo from './logo.svg';
+import './App.css';
+import { useState,useEffect } from 'react';
 
-const App = () => {
+function App() {
+  const [inpval, setInpval] = useState("");
+
+  function getData(event){
+     setInpval(event.target.value)
+  }
+   
+
+  
+
+  console.log(inpval)
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="App">
+      <label>Enter your Name:</label>
+      <br></br>
+      <br></br>
+      <br></br>
+      <input type="text"  onChange={getData}  />
+
+      <p >{`hello ${inpval}!`} </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
